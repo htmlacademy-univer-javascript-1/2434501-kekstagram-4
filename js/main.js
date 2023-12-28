8a04239a07d94126fe9004f15f7e3e6dbc7e6333
CreatFun <129187437+CreatFun@users.noreply.github.com>
2023-12-28 07:47:34 +0500

    12.12 устранение дребезга

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@ import './upload-photo/render-upload-form.js';
 import './network-api.js';
 import { getData } from './network-api.js';
 import { filterDefaultClick, renderThumbnails, filterRandomClick, filterDiscussedClick } from './render-thumbnails.js';
-import {showAlert} from './utils.js';
+import {debounce, showAlert} from './utils.js';
+
+const RERENDER_DELAY = 500;
 
 getData()
   .then((thumbnails) => {
     renderThumbnails(thumbnails);
-    filterDefaultClick(() => renderThumbnails(thumbnails));
-    filterRandomClick(() => renderThumbnails(thumbnails));
-    filterDiscussedClick(() => renderThumbnails(thumbnails));
+    filterDefaultClick(debounce(() => renderThumbnails(thumbnails), RERENDER_DELAY));
+    filterRandomClick(debounce(() => renderThumbnails(thumbnails), RERENDER_DELAY));
+    filterDiscussedClick(debounce(() => renderThumbnails(thumbnails), RERENDER_DELAY));
   })
   .catch(
     (err) => {
